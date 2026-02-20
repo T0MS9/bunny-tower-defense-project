0229e59d147532b2eaffabc71cfc158f8c630184
Neglect Team <joanequatrobontehg4@gmail.com>
2026-02-20 06:52:38 +0000

    Cursor Funcional
    obrigado Gemini

---
+++ BTDProject/Scenes/cursor.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Aqui dizemos ao Godot onde estão as tuas imagens
+var cursor_point = load("res://Assets/Sprites/Others/UI/Others/CursorPoint.png")
+var cursor_pressed = load("res://Assets/Sprites/Others/UI/Others/CursorPressed.png")
+
+func _input(event):
+	# Verifica se carregaste no botão do rato
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				# Quando carregas, muda para o ícone de 'Pressed'
+				Input.set_custom_mouse_cursor(cursor_pressed)
+			else:
+				# Quando soltas, volta para o ícone de 'Point'
+				Input.set_custom_mouse_cursor(cursor_point)
