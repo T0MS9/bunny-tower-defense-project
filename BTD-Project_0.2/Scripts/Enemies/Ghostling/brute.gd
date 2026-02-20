a1e7c9ac05d56e5abf7d5fe80b61bfb4fbc9061d
nke812 <dinisoliveira925@gmail.com>
2026-02-20 18:23:30 +0000

    Big organização

---
+++ BTD-Project_0.2/Scripts/Enemies/Ghostling/brute.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var speed = 250
+
+var vida_B = 5
+
+func _process(delta: float) -> void:
+	$"../ProgressBar".value = vida_B
+
+func _physics_process(delta):
+	var pf = get_parent() as PathFollow2D
+	pf.progress += speed * delta
+
+
+	if $"..".progress_ratio == 1:
+		get_tree().call_group("HP", "take_dmg", 1)
+		$"..".queue_free()
+
+func _on_button_mouse_entered() -> void:
+	var moedas = get_tree().current_scene.find_child("Moedas")
+	var valor_atual = int(moedas.text)
+	
+	vida_B = vida_B - 1
+	moedas.text = str(valor_atual + 1)
+	
+	if vida_B <= 0:
+
+		moedas.text = str(valor_atual + 4)
+		$"..".queue_free()
---
+++ BTD-Project_0.2/Scripts/Enemies/Ghostling/ghostling.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed = 450
+
+func _physics_process(delta):
+	var pf = get_parent() as PathFollow2D
+	pf.progress += speed * delta
+
+
+	if $"..".progress_ratio == 1:
+		get_tree().call_group("HP", "take_dmg", 5)
+		$"..".queue_free()
+
+func _on_button_mouse_entered() -> void:
+
+	var moedas = get_tree().current_scene.find_child("Moedas")
+	var valor_atual = int(moedas.text)
+	
+	moedas.text = str(valor_atual + 1)
+	$"..".queue_free()
