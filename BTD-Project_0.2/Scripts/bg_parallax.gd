8a722da8b9ffca05624856042d4fad2e09263295
Neglect Team <joanequatrobontehg4@gmail.com>
2026-02-21 00:54:07 +0000

    Cursor

--- BTD-Project_0.2/Scripts/bg_parallax.gd
+++ BTD-Project_0.2/Scripts/bg_parallax.gd
@@ -1,14 +1,14 @@
 extends Sprite2D
-
-@export var intensidade: float = 25.0 
-@export var suavizacao: float = 5.0
-
-@onready var posicao_original = position
-
-func _process(delta):
-	var centro_tela = get_viewport_rect().size / 2
-	var direcao_mouse = (get_global_mouse_position() - centro_tela).normalized()
-	
-	var alvo = posicao_original + (direcao_mouse * intensidade)
-	
-	position = position.lerp(alvo, suavizacao * delta)
+#
+#@export var intensidade: float = 25.0 
+#@export var suavizacao: float = 5.0
+#
+#@onready var posicao_original = position
+#
+#func _process(delta):
+	#var centro_tela = get_viewport_rect().size / 2
+	#var direcao_mouse = (get_global_mouse_position() - centro_tela).normalized()
+	#
+	#var alvo = posicao_original + (direcao_mouse * intensidade)
+	#
+	#position = position.lerp(alvo, suavizacao * delta)
--- BTD-Project_0.2/global.gd
+++ BTD-Project_0.2/global.gd
@@ -1,8 +1,21 @@
 extends Node
 
+# Aqui dizemos ao Godot onde estão as tuas imagens
+var cursor_point = load("res://Assets/Others/Others/CursorPoint.png")
+var cursor_pressed = load("res://Assets/Others/Others/CursorPressed.png")
+# Verifica se carregaste no botão do rato
+
 func _input(event):
 	if event.is_action_pressed("fullscreen_key"):
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+# Quando carregas, muda para o ícone de 'Pressed'
+				Input.set_custom_mouse_cursor(cursor_pressed)
+			else:
+				# Quando soltas, volta para o ícone de 'Point'
+				Input.set_custom_mouse_cursor(cursor_point)
