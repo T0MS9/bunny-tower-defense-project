bf035ac3c2fb082c1c4821051be4c4877618d9b4
nke812 <dinisoliveira925@gmail.com>
2026-03-13 09:58:40 +0000

    Slasher Ult & Slasher buff

--- BTD-Project_0.2/Scripts/Bunnies/slasher.gd
+++ BTD-Project_0.2/Scripts/Bunnies/slasher.gd
@@ -3,11 +3,21 @@ extends Node2D
 var mostrar_range = false
 var dmg_Slasher = 3
 
-# Usamos @onready para encontrar os nós assim que o jogo começa
+#@onready encontra os nós assim que o jogo começa
 @onready var moedas_label = get_tree().current_scene.find_child("Moedas", true, false)
 @onready var moedas_barra = get_tree().current_scene.find_child("PGB_M", true, false)
 
-# --- RESTO DO TEU CÓDIGO (DRAW E ATAQUE) ---
+@onready var SlasherNormal = preload("res://Assets/Bunnies/Slasher.png")
+@onready var SlasherUlt = preload("res://Assets/Others/Abilities & Utilities/SlasherUlt.png")
+
+func _process(delta: float) -> void:
+	if $Timer.time_left <= 0.3:
+		$Slasher.texture = (SlasherUlt)
+		
+	$ProgressBar.value = $Timer.time_left
+
+
+#DRAW RANGE E ATAQUE
 func _draw() -> void:
 	if mostrar_range:
 		var shape = $Range/CollisionRange.shape
@@ -25,11 +35,17 @@ func _on_insp_mouse_exited() -> void:
 
 func _on_timer_timeout() -> void:
 	var corpos = $Range.get_overlapping_bodies()
+	
 	for corpo in corpos:
 		if corpo.is_in_group("Ghostlings"):
 			atacar(corpo)
+			# Se for para atacar apenas UM de cada vez, coloca um 'break' aqui
+			break
+		
 
 func atacar(alvo):
 	if alvo.has_method("DMGED"):
 		$Slasher/AnimationPlayer.play("LuckyAction")
 		alvo.DMGED(dmg_Slasher)
+		$Timer.start()
+		$Slasher.texture = (SlasherNormal)
